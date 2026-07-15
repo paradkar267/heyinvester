f0b3a3005cdff6031fcf19b49ee6f78c93926415
delta student <yashparadkar63@gmail.com>
2026-07-15 18:49:43 +0530

    Fix: Typo in Blogs link URL in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -70,7 +70,7 @@ export default function Footer() {
               <div className="footer__link-pill-right"><svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><polyline points="9 18 15 12 9 6"/></svg></div>
             </Link>
             
-            <Link className="footer__link-pill" to="/blogs">
+            <Link className="footer__link-pill" to="/blog">
               <div className="footer__link-pill-left">
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path><path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path></svg>
                 Blogs
